b68c4209d2e3c17f05d27b8f04dedf160d007c25
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-17 02:33:01 -0300

    :sparkles: JWT functionality

--- Autenticacao/api/src/app.controller.ts
+++ Autenticacao/api/src/app.controller.ts
@@ -2,19 +2,15 @@ import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
