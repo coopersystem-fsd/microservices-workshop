a1f3db7f072e9167b4155713b3ad4e1c28adedd4
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-17 01:56:48 -0300

    :sparkles: Implementing Passport strategies

--- Autenticacao/api/src/auth/auth.service.ts
+++ Autenticacao/api/src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (!user) {
+      return null;
+    }
+    if (user.password != pass) {
+      return false;
+    }
+    const { password, ...result } = user;
+    return result;
+  }
+}
