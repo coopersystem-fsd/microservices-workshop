e7a28795e6a7cad3af51a6d783cf8916670bdc03
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-17 02:46:45 -0300

    :sparkles: Add route to get profile

--- Autenticacao/api/src/app.controller.ts
+++ Autenticacao/api/src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -12,5 +13,11 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
