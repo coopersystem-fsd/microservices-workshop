864bb673735d3c0f4bb4dc6faa15806814236d48
Lucas de Freitas <lucasffm@gmail.com>
2020-06-17 20:24:46 -0300

    :sparkles: Add Auth middleware
    Co-authored-by: Matheus Barriolo <matheusbarriolo@gmail.com>

--- Cardapio/src/app.module.ts
+++ Cardapio/src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  HttpModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PlatesModule } from './modules/plates/plates.module';
 import * as config from './config/database/ormconfig';
+import { AuthMiddleware } from './core/middlewares/auth.middleware';
+import { PlatesModule } from './modules/plates/plates.module';
+import { PlatesController } from './modules/plates/plates.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), PlatesModule],
+  imports: [TypeOrmModule.forRoot(config), HttpModule, PlatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(PlatesController);
+  }
+}
--- Cardapio/src/modules/plates/plates.controller.ts
+++ Cardapio/src/modules/plates/plates.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { PlatesService } from './plates.service';
 
 @Controller('plates')
 export class PlatesController {
-
   constructor(private plateService: PlatesService) {}
-  
+
   @Get('')
-  public async index() {
+  public async index(@Req() req) {
     return this.plateService;
   }
 }
