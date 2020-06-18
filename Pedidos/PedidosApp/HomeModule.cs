9d733d4a1fea32a10ba1ce18f5a2a46b53812c0e
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2020-06-18 19:26:21 -0300

    feat: adicionando código base

---
+++ Pedidos/PedidosApp/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿namespace Pedidos
+{
+    using Carter;
+    using Microsoft.AspNetCore.Http;
+
+    public class HomeModule : CarterModule
+    {
+        public HomeModule()
+        {
+            Get("/", async(req, res) => await res.WriteAsync("Hello from Carter!"));
+        }
+    }
+}
