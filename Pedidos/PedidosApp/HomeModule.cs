a45be6587cd003c008ac91cf82904eba1beec9bd
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2020-06-18 21:14:26 -0300

    feat: implementando endpoint para status dos pedidos

--- Pedidos/PedidosApp/HomeModule.cs
+++ Pedidos/PedidosApp/HomeModule.cs
@@ -1,13 +1,43 @@
 ﻿namespace Pedidos
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Json;
+    using System.Threading.Tasks;
     using Carter;
     using Microsoft.AspNetCore.Http;
+    using Newtonsoft.Json.Linq;
+    using Npgsql;
 
     public class HomeModule : CarterModule
-    {
+    {        
         public HomeModule()
         {
-            Get("/", async(req, res) => await res.WriteAsync("Hello from Carter!"));
+            var connString = "Host=localhost;Username=pedidos_usr;Password=pedidos_pass;Database=pedidos_db";
+
+            Get("/status_pedidos", async(req, res) => {
+                
+                await using var conn = new NpgsqlConnection(connString);
+                await conn.OpenAsync();
+
+                List<Object> status = new List<Object>();
+    
+                await using (var cmd = new NpgsqlCommand("SELECT * FROM status_pedidos", conn))
+                await using (var reader = await cmd.ExecuteReaderAsync()) {
+                    
+                    while (await reader.ReadAsync()) {
+                        status.Add(new { 
+                            status = reader.GetString(0),
+                            descricao = reader.GetString(1) 
+                        });
+                    }
+
+                    string jsonString;
+                    jsonString = JsonSerializer.Serialize(status);
+
+                    await res.WriteAsync(jsonString);
+                }
+            });
         }
     }
 }
