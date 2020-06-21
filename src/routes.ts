1af1dc9b916454a4da4893be2b063a07935bcf74
Camilo Fernandes <camilo.analista@gmail.com>
2020-06-21 02:40:58 -0300

    :bug: restaurando body

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'https://deno.land/x/oak/mod.ts'
 const routes = new Router();
 
 routes.get('/', ({params, response}: {params: any; response: any}) => {
+    response.body = {message: 'Hello world!'};
     response.status = 200;
 });
 
