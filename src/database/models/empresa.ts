5d679af47a3ebbe7a77cc94357803b96a04fe934
Camilo Fernandes <camilo.analista@gmail.com>
2020-06-21 14:13:27 -0300

    :card_file_box: Criando modelo da base

--- src/database/models/empresa.ts
+++ src/database/models/empresa.ts
@@ -1,10 +1,22 @@
-import { Model } from 'https://deno.land/x/denodb/mod.ts';
+import { Model, DataTypes } from 'https://deno.land/x/denodb/mod.ts';
 
 class Empresa extends Model {
   static fields = {
     _id: {
       primaryKey: true,
     },
+    razaoSocial: {
+      type: DataTypes.STRING
+    },
+    nomeFantasia: {
+      type: DataTypes.STRING
+    },
+    cnpj: {
+      type: DataTypes.STRING
+    },
+    foto: {
+      type: DataTypes.STRING
+    }
   };
 }
 
