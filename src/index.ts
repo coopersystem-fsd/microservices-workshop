f82767f0005cc4535691468a1d9640bb3e6389f6
Camilo Fernandes <camilo.analista@gmail.com>
2020-06-21 05:29:51 -0300

    :fire: Remove code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { Database } from 'https://deno.land/x/denodb/mod.ts';
 import router from './routes.ts';
 
 const env = Deno.env.toObject()
 const PORT = parseInt(env.PORT);
-
-const MONGODB_URI = env.MONGODB_URI;
-
-const db = new Database('mongo', {
-    uri: MONGODB_URI,
-    database: 'empresas',
-});
 
 const app = new Application();
 
